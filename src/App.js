6701b497f7c9e24eaa49d6a56e037a70a469fd8d
vivipolli <pollivivian@gmail.com>
2020-06-26 08:58:07 -0300

    add api functions

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import api from './services/api';
 import "./styles.css";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [title, setTitle] = useState('repo teste');
+  const [url, setUrl] = useState('www.github.com/vivipolli');
+  const [techs, setTechs] = useState(['node', 'react', 'postgresql']);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setItems(response.data)
+    })
+  },[])
+
   async function handleAddRepository() {
-    // TODO
+    const data = {
+      title,
+      url,
+      techs
+    };
+    const response = await api.post('repositories', data)
+    const project = response.data;
+    setItems([...items, project]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+    const projects = items.filter(rep => rep.id !== id);
+    setItems(projects);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {items.map(item => 
+        <li key={item.id}>
+          {item.title}
+          <button onClick={() => handleRemoveRepository(item.id)}>
             Remover
           </button>
         </li>
+      )}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
